0c50e9b7e2bc80bc9a74a58303f0a2b0df540754
Joaobachmann <joaobachmannalun@gmail.com>
2026-03-24 14:32:33 -0300

    feat:Arrumando a model veiculo

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -4,8 +4,8 @@ from django.db import models
 class Veiculo(models.Model):
     ano = models.IntegerField(blank=True, default=0)
     preco = models.DecimalField(max_digits=7, decimal_places=2, blank=True, default=0)
-    modelo = models.ForeignKey('Modelo', blank=True, null=True, related_name='veiculos')
-    cor = models.ForeignKey('Cor', blank=True, null=True, related_name='veiculos')
+    modelo = models.ForeignKey('Modelo', on_delete=models.PROTECT, blank=True, null=True, related_name='veiculos')
+    cor = models.ForeignKey('Cor', on_delete=models.PROTECT, blank=True, null=True, related_name='veiculos')
     acessorios = models.ManyToManyField('Acessorio', blank=True, related_name='veiculos')
 
     def __str__(self):
